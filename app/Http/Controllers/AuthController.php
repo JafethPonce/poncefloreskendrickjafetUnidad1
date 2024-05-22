8b81e470c309a44a28c606e2bbd525acc51ed3ea
Ivan Alejandro <ivanalejandro.lopez07@gmail.com>
2024-05-22 02:12:03 -0600

    Pagina de ayuda y los controladores de usuarios y auth

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login()
+    {
+
+        return 'Login';
+    }
+
+    public function registro()
+    {
+
+        return 'registro';
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,45 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UsuariosController;
+use App\Models\Usuarios;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+
+Route::get('/', function(){
+    return view('welcome');
+});
+
+Route::get('/ayuda', function(){
+    return view('ayuda');
+})->name('ayuda');
+
+
+Route::controller(UsuariosController::class)->group(function(){ 
+
+});
+
+
+
+Route::controller(AuthController::class)->group(function(){
+
+    Route::get('/auth/login',           'login')->name('auth.login');
+    Route::get('/auth/registro',        'registro')->name('auth.registro');
+
+
+    Route::post('/auth/loginF',          'loginF')->name('auth.loginF');
+    Route::post('/auth/registroF',          'loginF')->name('auth.registroF');
+
+});
+
